b8ef1213ac45cf98f56d067a1e92c721f90ab3ae
rod <rod@roddickinson.net>
2021-05-04 22:39:56 +0100

    added raycast

--- Assets/Scripts State Machine/State.cs
+++ Assets/Scripts State Machine/State.cs
@@ -91,6 +91,7 @@ public class Idle: State
 // SetTarget state
 public class SetTarget : State {
 	int i = 0;
+	
 	public SetTarget (GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _cube, Transform _bay, AgentHealth _health) : base (_npc, _agent, _anim, _cube, _bay, _health)
 	{
 		name = STATE.SETTARGET;
@@ -116,21 +117,23 @@ public class SetTarget : State {
 
 		// find cube
 		RaycastHit hit;
-		Vector3 source = new Vector3 (agent.transform.position.x, agent.transform.position.y + 0.5f, agent.transform.position.z);
-		Vector3 angle = Vector3.forward;
+		Vector3 source = new Vector3 (agent.transform.position.x, agent.transform.position.y + 1.5f, agent.transform.position.z);
+		Vector3 angle = agent.transform.TransformDirection (Vector3.forward);
 		i += 1;
 		if (i == 1) {
-			angle = (Vector3.forward + Vector3.right).normalized;
+			angle = agent.transform.TransformDirection (Vector3.forward);
+			//angle = agent.transform.TransformDirection ((Vector3.forward + Vector3.right).normalized);
 		} else if (i == 2) {
-			angle = Vector3.forward;
+			angle = agent.transform.TransformDirection (Vector3.forward);
 		} else if (i == 3) {
-			angle = (Vector3.forward - Vector3.right).normalized;
+			angle = agent.transform.TransformDirection (Vector3.forward);
+			//angle = agent.transform.TransformDirection ((Vector3.forward - Vector3.right).normalized);
 			i = 0;
 		}
 		Ray ray = new Ray (source, angle);
-		if (Physics.Raycast (ray, out hit, 40)) {
+		if (Physics.SphereCast (ray, 3.0f, out hit, 60)) {
 			Debug.DrawRay (source, angle * hit.distance, Color.red);
-			//Debug.Log (hit.collider.name);
+			Debug.Log (hit.collider.name);
 			if (hit.collider.tag == "cube") {
 				Debug.Log (hit.collider.name);
 				cube.position = GameObject.Find (hit.collider.name).transform.position;
@@ -146,6 +149,7 @@ public class SetTarget : State {
 		base.Exit ();
 	}
 
+	// return random position to move to
 	Vector3 Wander()
 	{
 		Vector3 randomDirection = Random.insideUnitSphere * 10;
