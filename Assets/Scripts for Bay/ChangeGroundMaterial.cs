f71458b9dc834fc4dc7b9dcf8816c06f209c954f
rod <rod@roddickinson.net>
2021-05-11 22:57:21 +0100

    added markers

--- Assets/Scripts for Bay/ChangeGroundMaterial.cs
+++ Assets/Scripts for Bay/ChangeGroundMaterial.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class ChangeGroundMaterial : MonoBehaviour
 {
     public GameObject ground;
-    Material m_GroundMaterial; //cached on Awake()
-    public Material goalScoredMaterial;
+	public GameObject marker;
+	Material m_GroundMaterial; //cached on Awake()
+	Material m_MarkerMaterial; //cached on Awake()
+	public Material goalScoredMaterial;
+	public Material goalScoredMarkerMaterial;
     Renderer m_GroundRenderer;
-    GoalDetect GoalDetect;
+	Renderer m_MarkerRenderer;
+	GoalDetect GoalDetect;
 
     // Start is called before the first frame update
     void Start()
     {
         // Get the ground renderer so we can change the material when a goal is scored
         m_GroundRenderer = ground.GetComponent<Renderer>();
-        // Starting material
-        m_GroundMaterial = m_GroundRenderer.material;
-        // get goal detect script
-        GoalDetect = GetComponent<GoalDetect>();
+		m_MarkerRenderer = marker.GetComponent<Renderer> ();
+		// Starting material
+		m_GroundMaterial = m_GroundRenderer.material;
+		m_MarkerMaterial = m_MarkerRenderer.material;
+		// get goal detect script
+		GoalDetect = GetComponent<GoalDetect>();
     }
 
     void OnTriggerEnter(Collider other) 
@@ -26,15 +32,17 @@ public class ChangeGroundMaterial : MonoBehaviour
         // Touched goal and goal name
         if (other.gameObject.CompareTag("cube")) {
             // Swap ground material for a bit to indicate we scored.
-            StartCoroutine(GoalScoredSwapGroundMaterial(goalScoredMaterial, 0.5f));
+            StartCoroutine(GoalScoredSwapGroundMaterial(goalScoredMaterial, goalScoredMarkerMaterial, 0.5f));
         }
     }
 
     /// Swap ground material, wait time seconds, then swap back to the regular material.
-    public IEnumerator GoalScoredSwapGroundMaterial(Material mat, float time)
+    public IEnumerator GoalScoredSwapGroundMaterial(Material groundMaterial, Material markerMaterial, float time)
     {
-        m_GroundRenderer.material = mat;
-        yield return new WaitForSeconds(time); // Wait for 2 sec
+        m_GroundRenderer.material = groundMaterial;
+		m_MarkerRenderer.material = markerMaterial;
+		yield return new WaitForSeconds(time); // Wait for 2 sec
         m_GroundRenderer.material = m_GroundMaterial;
-    }
+		m_MarkerRenderer.material = m_MarkerMaterial;
+	}
 }
