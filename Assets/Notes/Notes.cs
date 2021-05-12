463e0f7ac07dd31173287605d18e7bb8eb2e4852
rod <rod@roddickinson.net>
2021-05-12 22:47:39 +0100

    added agents

--- Assets/Notes/Notes.cs
+++ Assets/Notes/Notes.cs
@@ -7,14 +7,18 @@
 //BAY
 
 //AGENT
-//- still gets stuck in a SEARCH hit cube PUSH loop
-//- slow down as apparoach bay so not to 'bang' other cubes
-//- keep rigid body constraints until PUSH is over - hmm have I done this..?Nope
 //- first ‘wander’ head toward the centre of the nav mesh
+//- slow down as approach bay so not to 'bang' other cubes
+//- set timer from SETTARGET so agents dont get stuck trying to get one cube.
+//- still gets stuck in a SEARCH hit cube PUSH loop - investigate - timer not working
+//- keep rigid body constraints until PUSH is over - hmm have I done this..?Nope
+
 //- change agent speed based on health
 //- add cooperation and compete coefficients to state (like health)
-//
 
+
+// GANTRY
+//- fix pick up cube - change move position to parent transform
 
 // ADJUSTMENTS
 // previously Changed the cube from mass 1, Drag 0.5, TO mass 1.5, Drag 1  - to make it harder for the agent
