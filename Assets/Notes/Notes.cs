4da7586162061a718be0dc8d2caf2cb3da71152f
rod <rod@roddickinson.net>
2021-05-16 01:26:30 +0100

    added Breathless and Death state

--- Assets/Notes/Notes.cs
+++ Assets/Notes/Notes.cs
@@ -1,7 +1,5 @@
 ﻿// TO DO
 
-// ANIMATIONS
-// Death animation
 
 //GANTRY CRANE
 // make crane more efficient
@@ -11,13 +9,16 @@
 
 //BAY MARKER
 //- correlate emmission material brightness with Agent health - dead agent = black marker
+//- design better marker
 
 //AGENT
 //- fix jittering animations
 //- fix / new push animation
 //- fix PantingInt 2, ProneInt 1 & PantingInt 2, ProneInt 0
-//- keep rigid body constraints until PUSH is over - hmm have I done this..?Nope
+//- keep rigid body constraints until PUSH is over - hmm have I done this..? good enough
 //- add ragdoll state to dead agents
+
+// create 100% cooperative states
 
 //- change agent speed based on health
 //- add cooperation and compete coefficients to state (like health) .. 0.0 to 1.0 float
