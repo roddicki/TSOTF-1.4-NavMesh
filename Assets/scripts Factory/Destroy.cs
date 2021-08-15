3a402acc83d583cc7ad3f2d7dd94b5c75b9b80d0
rod <rod@roddickinson.net>
2021-08-15 01:17:27 +0100

    adding ragdolls

---
+++ Assets/scripts Factory/Destroy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // destroy cubes
+    public void DestroyCubes()
+    {
+        // get all cubes
+        GameObject[] cubes = GameObject.FindGameObjectsWithTag ("cube");
+        // destroy all cubes
+        foreach (GameObject cube in cubes) {
+            if (cube.name != "CubeMaster")
+            {
+                Destroy(cube);
+            }
+            
+        }
+    }
+
+    // destroy agents
+    public void DestroyAgents()
+    {
+        // get all agents
+        GameObject[] agents = GameObject.FindGameObjectsWithTag ("agent");
+        // destroy all agents
+        foreach (GameObject agent in agents) {
+            if (agent.name != "Agent0")
+            {
+                Destroy(agent);
+            }
+        }
+    }
+}
--- Assets/scripts Factory/GameManager.cs
+++ Assets/scripts Factory/GameManager.cs
@@ -3,21 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-   // NOT USED AT MO
-   // todo
    
-   // get cable and weight attached
-   // lower and retract cable and weight
-   // get magnet effect working / follow enemy
-
+    private Destroy destroy;
+    public float timeRemaining;
+    private bool startTimer;
 
     // Start is called before the first frame update
     void Start() {
-        
+        destroy = GetComponent<Destroy>();
+		startTimer = true;
     }
 
     // Update is called once per frame 
     void Update() {
-       
+        if (TimeUp())
+        {
+            destroy.DestroyCubes();
+            destroy.DestroyAgents();
+        }
+       //Timer();
     }
+
+    // timer
+	private bool TimeUp()
+	{
+		if (startTimer) {
+            if (timeRemaining > 0) {
+                timeRemaining -= Time.deltaTime;
+				return false;
+            }
+            else {
+                timeRemaining = 0;
+                startTimer = false;
+				return true;
+            }
+        } 
+		else {
+			return false;
+		}
+	}
 }
